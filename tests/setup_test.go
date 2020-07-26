5d3df17fddf8bd013397aed7097ef67046073277
Lgdev07 <luan_saopaulo06@hotmail.com>
2020-07-26 13:53:25 -0300

    [FIX] - Fix circleci

--- tests/setup_test.go
+++ tests/setup_test.go
@@ -9,24 +9,48 @@ import (
 	"github.com/Lgdev07/crud_api/controllers"
 	"github.com/Lgdev07/crud_api/models"
 	"github.com/jinzhu/gorm"
+	"github.com/joho/godotenv"
 )
 
 var app = controllers.App{}
 var storeInstance = models.Store{}
 
 func TestMain(m *testing.M) {
-	Database()
-
+	//Since we add our .env in .gitignore, Circle CI cannot see it, so see the else statement
+	if _, err := os.Stat("./../.env"); !os.IsNotExist(err) {
+		var err error
+		err = godotenv.Load(os.ExpandEnv("./../.env"))
+		if err != nil {
+			log.Fatalf("Error getting env %v\n", err)
+		}
+		Database()
+	} else {
+		CIBuild()
+	}
 	os.Exit(m.Run())
+}
 
+//When using CircleCI
+func CIBuild() {
+	var err error
+	DBURL := fmt.Sprintf(`host=localhost port=5432 user=lgdev07 
+	dbname=crud_api_test sslmode=disable password=docker`)
+	app.DB, err = gorm.Open("postgres", DBURL)
+	if err != nil {
+		fmt.Printf("Cannot connect to postgres database\n")
+		log.Fatal("This is the error:", err)
+	} else {
+		fmt.Printf("We are connected to the postgres database\n")
+	}
 }
 
 func Database() {
 
 	var err error
-
-	DBURL := fmt.Sprintf(`host=localhost port=5432 user=postgres 
-	dbname=crud_api_test sslmode=disable password=docker`)
+	DBURL := fmt.Sprintf(`host=%s port=%s user=%s dbname=%s sslmode=disable 
+	password=%s`, os.Getenv("TEST_DB_HOST"), os.Getenv("DB_PORT"),
+		os.Getenv("DB_USER"), os.Getenv("TEST_DB_NAME"),
+		os.Getenv("DB_PASSWORD"))
 
 	app.DB, err = gorm.Open("postgres", DBURL)
 	if err != nil {
