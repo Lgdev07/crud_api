497143b6da962fcf4d5f355b1234c406ec201a5b
Lgdev07 <luan_saopaulo06@hotmail.com>
2020-07-28 08:21:52 -0300

    [IMP] - Add more tests

--- tests/model_stores_test.go
+++ tests/model_stores_test.go
@@ -18,12 +18,38 @@ func TestAllStores(t *testing.T) {
 		log.Fatal(err)
 	}
 
-	users, err := storeInstance.GetStores(app.DB)
+	stores, err := storeInstance.GetStores(app.DB)
 	if err != nil {
 		t.Errorf("There was an error when getting all stores, err: %v\n", err)
 		return
 	}
 
-	assert.Equal(t, len(*users), 2)
+	assert.Equal(t, len(*stores), 2)
+	assert.Equal(t, (*stores)[0].Name, "Store 2")
+	assert.Equal(t, (*stores)[0].Type, "Type 2")
+	assert.Equal(t, (*stores)[0].Active, true)
 
+}
+
+func TestOneStore(t *testing.T) {
+	err := refreshStoreTable()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = seedOneStore()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	store, err := storeInstance.GetStores(app.DB)
+	if err != nil {
+		t.Errorf("There was an error when getting a store, err: %v\n", err)
+		return
+	}
+
+	assert.Equal(t, len(*store), 1)
+	assert.Equal(t, (*store)[0].Name, "Store 1")
+	assert.Equal(t, (*store)[0].Type, "Type 1")
+	assert.Equal(t, (*store)[0].Active, true)
 }
